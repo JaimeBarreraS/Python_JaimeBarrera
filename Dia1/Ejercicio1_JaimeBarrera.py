9296366bfc00ef9989c68fd3ca16f47085db4f79
JaimeBarreraS <jaenba10@gmail.com>
2024-04-09 13:02:26 -0500

    commit #3 python

---
+++ Dia1/Ejercicio1_JaimeBarrera.py
@@ -0,0 +1,38 @@
+jaime="Hola, me llamo Jaime"
+jaime1="Tengo"
+jaime2= 24 
+jaime3="años"
+print(jaime)
+print(jaime1)
+print(jaime2) 
+print(jaime3) 
+
+#------------------------------------
+#------DIA 1 CHEST SHEET PYTHON------
+#------------------------------------
+
+#Imprimir hola mundo
+print("¡¡Hola, mundoooooo!!")
+#Datos primitivos
+
+#Numero 
+numerito = 1 #nombrevariable = valor
+print(numerito) #imprimir (variable)
+print(type(numerito)) #imprimir (tipo(valor))
+
+#decimal
+numeritoDecimal = 1.23456 #nombrevariable = valor
+print(numeritoDecimal) #imprimir (variable)
+print(type(numeritoDecimal)) #imprimir (tipo(valor))
+
+#booleano
+mibooleanito=True #variable del boleano=verdadero
+print(mibooleanito) #imprimir (variable)
+print(type(mibooleanito)) #imprimir (type(valor))
+
+#Cadena de texto
+texto="Camper Jaime Barrera" #Variables texto= valor
+print(texto) #imprimir (variable)
+print(type(texto))#imprimir (type(valor))
+
+#Desarrollado por Jaime Barrera - C.C 1.093.925.253
---
+++ Dia1/Ejercicio2_JaimeBarrera.py
@@ -0,0 +1,51 @@
+#TRABAJO PEDROOOO
+print("")
+print("Buenas mi primer Trabajo")
+print("")
+
+#Ingresar por teclado la imformacion/////
+print("Ingresa tu nombre")
+ingresar=input()
+print("")
+#Convertir de variables
+#TEXTO A NUMERO/////
+print("String a Numero")
+Texto="Jaime Barrera"
+print(Texto)
+print(type(Texto))
+print("String a Numero")
+print("Jaime Barrera a 24")
+Texto=24
+print(Texto)
+print(type(Texto))
+print("")
+#NUMERO A DECIMAL
+print("Numero a Decimal")
+print(Texto)
+print(type(Texto))
+print("Numero a Decimal")
+print("24 a 1.234")
+Texto=1.234
+print(Texto)
+print(type(Texto))
+print("")
+
+#Bucles for y while
+print("Bucle for")
+for i in range (4):
+    print(i)
+    print("Soy camper")
+
+print("Bucle while")
+n = 1
+while n <= 5:
+    print(n)
+    n = n + 1
+
+print("")
+
+#funciones 
+
+
+
+#Desarrollado por Jaime Barrera - C.C 1.093.925.253
