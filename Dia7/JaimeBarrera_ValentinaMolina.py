d9bcccc1e31126c6f7d66e29a9d5b6dc9a22fb54
JaimeBarreraS <jaenba10@gmail.com>
2024-04-17 22:19:23 -0500

    JaimeAndValentina

--- Dia7/JaimeBarrera_ValentinaMolina.py
+++ Dia7/JaimeBarrera_ValentinaMolina.py
@@ -23,12 +23,13 @@ diccionario = {
 # Imprimir mensaje de bienvenida
 
 print("\n¡¡BIENVENIDOS A SUPER-VALJAI!!")
+print("")
 
 
 # Mostrar información de los productos disponibles
 for clave in diccionario:               #este for repite sobre cada clave del diccionario
     producto = diccionario[clave]       #se obtiene el valor que tiene cada clave (nombre,precio,cantidad)
-    print(f"\nProducto: {producto['Nombre']} - Precio: ${producto['Precio']} - Cantidad: {producto['Cantidad']}") #hago un print contatenado para mostrar nombre,precio y cantidad
+    print(f"Producto: {producto['Nombre']} - Precio: ${producto['Precio']} - Cantidad: {producto['Cantidad']}") #hago un print contatenado para mostrar nombre,precio y cantidad
 
 # Mostrar la clave de cada producto
 print("\nCLAVES DE PRODUCTOS")
@@ -54,45 +55,44 @@ carrito = []
 # Bucle principal del programa
 while opcion != 4: #creo un while que si es diferente a 4 me haga las otras opciones
 
-    # Opción 1: Agregar productos al carrito
-    if opcion == 1: #si opcion es igual a 1 
+    if opcion == 1: # Si la opción ingresada es 1 (Agregar productos al carrito)
         codigo = input("Ingrese la clave del producto: ") #le pido que ingrese la clave del producto
-        if codigo in diccionario:
-            producto = diccionario[codigo]
-            cantidad = int(input(f"Ingrese la cantidad de {producto['Nombre']}: "))
-            if cantidad <= producto['Cantidad']:
-                carrito.append((codigo, cantidad))
+        if codigo in diccionario:# Si el código ingresado existe en el diccionario de productos
+            producto = diccionario[codigo] # Se obtiene el diccionario anidado correspondiente al producto
+            cantidad = int(input(f"Ingrese la cantidad de {producto['Nombre']}: ")) # Se solicita al usuario que ingrese la cantidad del producto, usando su nombre
+            if cantidad <= producto['Cantidad']:# Si la cantidad ingresada no excede la cantidad disponible
+                carrito.append((codigo, cantidad))# Se agrega una tupla con el código y la cantidad al carrito
                 print("")
-                print("Producto agregado al carrito.")
+                print("Producto agregado al carrito.")# Se confirma que el producto se ha agregado al carrito
             else:
-                print("No hay suficiente cantidad disponible.")
+                print("No hay suficiente cantidad disponible.")# Si no hay suficiente cantidad disponible, se muestra este mensaje
         else:
-            print("Código de producto inválido.")
+            print("Código de producto inválido.")# Si el código ingresado no existe en el diccionario, se muestra este mensaje
 
     # Opción 2: Ver contenido del carrito
-    elif opcion == 2:
-        if carrito:
+    elif opcion == 2:# Si la opción ingresada es 2 (ver productos al carrito)
+        if carrito: # Si 'carrito' no está vacío, se ejecuta 
             print("Contenido del carrito:")
-            for item in carrito:
-                codigo, cantidad = item
-                producto = diccionario[codigo]
-                valor = producto['Precio'] * cantidad
-                print(f"{producto['Nombre']}: {cantidad} unidades - Valor: ${valor}")
+            for elemento in carrito:# repite sobre cada elemento en 'carrito'
+                codigo, cantidad = elemento # realiza el codigo y la cantidad del elemento
+                producto = diccionario[codigo] # Obtiene el diccionario correspondiente al código del producto
+                valor = producto['Precio'] * cantidad  # Calcula el valor total del producto multiplicando el precio por la cantidad
+                print(f"{producto['Nombre']}: {cantidad} unidades - Valor: ${valor}") # Imprime el nombre del producto, la cantidad y el valor total
         else:
-            print("El carrito está vacío.")
+            print("El carrito está vacío.") # Si 'carrito' está vacío, se ejecuta esta linea
 
     # Opción 3: Calcular valor de la compra
-    elif opcion == 3:
-        total = 0
-        for item in carrito:
-            codigo, cantidad = item
-            producto = diccionario[codigo]
-            total += producto['Precio'] * cantidad
-        print(f"El valor total de la compra es: ${total}")
+    elif opcion == 3:# Si la opción ingresada es 3 (calcular el valor de la compra)
+        total = 0 # Inicializa la variable total en 0
+        for elemento in carrito: # repite sobre cada elemento en la lista carrito
+            codigo, cantidad = elemento # Desempaca la tupla en las variables codigo y cantidad
+            producto = diccionario[codigo] # Obtiene el producto del diccionario usando el código como clave
+            total += producto['Precio'] * cantidad # Suma al total el precio del producto multiplicado por la cantidad
+        print(f"El valor total de la compra es: ${total}") # Imprime el valor total de la compra
 
     # Opción inválida
     else:
-        print("\nOpción inválida.")
+        print("\nOpción inválida.") # Imprime un mensaje de opción inválida
 
     # Mostrar el menú de opciones nuevamente
     print("\nESTAS EN EL MENU PRINCIPAL")
@@ -107,28 +107,21 @@ while opcion != 4: #creo un while que si es diferente a 4 me haga las otras opci
 
 # Mensaje de despedida
 
-print("\n GRACIAS POR ELEGIR A SUPER-VALJAI")
-print("\n Esta es tu factura o resumen de  compra")
-print("")
-
-if opcion == 4:
-        if carrito:
-            print("LOS PRODUCTOS DE TU COMPRA SON: ")
-            for item in carrito:
-                codigo, cantidad = item
-                producto = diccionario[codigo]
-                valor = producto['Precio'] * cantidad
-                print(f"{producto['Nombre']}: {cantidad} unidades - Valor: ${valor}")
-        else:
-            print("EN EL MEMENTO NO HAZ COMPRADO NINGUN PRODUCTO")
-        
-        if opcion == 4:
-            total = 0
-        for item in carrito:
-            codigo, cantidad = item
-            producto = diccionario[codigo]
-            total += producto['Precio'] * cantidad
-        print(f"El valor total de tu compra es: ${total}")
+if carrito:# Verifica si la lista carrito contiene elementos
+    print("\n GRACIAS POR ELEGIR A SUPER-VALJAI") # Imprime un mensaje de agradecimiento
+    print("\n Esta es tu factura o resumen de  compra") # Imprime resumen de la factura
+    print("")
+    total = 0
+    for elemento in carrito:# repite sobre cada elemento en la lista carrito
+        codigo, cantidad = elemento # Desempaca la tupla en las variables codigo y cantidad
+        producto = diccionario[codigo] # Obtiene el producto del diccionario usando el código como clave
+        valor = producto['Precio'] * cantidad # Calcula el valor del producto multiplicando el precio por la cantidad
+        total += valor# Suma el valor del producto al total
+        print(f"{producto['Nombre']}: {cantidad} unidades - Valor: ${valor}") # Imprime el nombre del producto, la cantidad y el valor
+    print("")
+    print(f"Total a pagar: ${total}")# Imprime el total a pagar
+else:
+    print("EN EL MEMENTO NO HAZ COMPRADO NINGUN PRODUCTO")# Imprime un mensaje indicando que no se han comprado productos
 
 print("\n¡Gracias por su compra!")
 #Jaime Barrera CC.1093925253 Valentida Molina CC.1007109135
