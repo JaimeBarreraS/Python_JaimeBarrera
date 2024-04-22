f80a22d13ad5020c86c2a2a4a3e6f90aa6fc5208
JaimeBarreraS <jaenba10@gmail.com>
2024-04-22 12:46:17 -0500

    Commit #11 Python

---
+++ Dia10/Ejercicio1.py
@@ -0,0 +1,21 @@
+#PRIMER EJEMPLO
+numero=[1,3,5,6]
+print(input("Ingresa el numero: "))
+print(numero)
+print(numero.index(5))
+
+#SEGUNDO EJEMPLO
+numero.append(int(input("Ingrese el numero: ")))
+numero1=sorted(numero)
+print(numero1)
+print(numero1.index(2))
+
+#TERCER EJEMPLO
+num=[1,3,5,6]                                    
+num.append(int(input("Ingrese el numero: ")))  
+numero2=sorted(num)
+print(numero2)
+print(numero2.index(7))
+
+#Hecho por Jaime Barrera cc 1093925253
+#Lo hice tal como esta en ejemplo
