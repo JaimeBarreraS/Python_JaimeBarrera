cdbeaa088e318fd9773a42988d591b2fc516a1a7
JaimeBarreraS <jaenba10@gmail.com>
2024-04-24 16:12:36 -0500

    Commit #13 Python

--- Dia11/Trabajo.py
+++ Dia11/Trabajo.py
@@ -35,17 +35,27 @@ while goku: #se abre while mientras goku sea true
                     for clave, valor in i.items(): #este for me permite ver la informacion de de los datos con clave y valor
                         print(f"{clave}: {valor}") #imprimo clave y valor de los datos de type 
 
-        elif opcion == 2:# Crear nuevo dato 
-            Dato_N = {'type': tipo} #creo un nuevo diccionario  Dato_N con una clave 'type' el valor es de la variable tipo (que ingreso arriba)
-            for i in datos:#se repite en la lista datos
-                if i['type'] == tipo:#se verifica si el valor de la clave type del diccionario es igual al valor de la variable tipo
-                    for clave in i.keys():#se repite sobre todas las claves del diccionario utilizando i.keys()
-                        if clave != 'type':# verifica si la clave actual es diferente de la cadena 'type'
-                            valor = input(f"Ingrese el valor para '{clave}': ") #se ingrese un valor para las demas claves
-                            Dato_N[clave] = valor #se agrega la clave y su valor al nuevo diccionario Dato_N
-            datos.append(Dato_N)#se agrega Dato_N a la lista datos
+        elif opcion == 2:  # Crear nuevo dato
+            Dato_N = {'type': tipo}  # Crea un nuevo diccionario Dato_N con una clave 'type' y el valor de la variable tipo
+            for i in datos:  # Itera sobre la lista datos
+                if i['type'] == tipo:  # Si el valor de la clave 'type' coincide con el tipo seleccionado
+                    for clave in i.keys():  # repite sobre todas las claves del diccionario
+                        if clave != 'type':  # Si la clave actual es diferente de 'type'
+                            valor = input(f"Ingrese el valor para '{clave}': ")  # Solicita al usuario el valor para esa clave
+                            if valor:  # Si el usuario ingresa un valor
+                                Dato_N[clave] = valor  # Agrega la clave y el valor al nuevo diccionario Dato_N
+                            else:  # Si el usuario no ingresa un valor
+                                sub_claves = input(f"¿Desea agregar subclaves para '{clave}'? (si/no): ")  # Pregunta si desea agregar subclaves
+                                if sub_claves.lower() == 'si':  # Si la respuesta es si
+                                    Dato_N[clave] = {}  # Inicializa la clave con un diccionario vacío
+                                    while True:  # Bucle para agregar subclaves
+                                        sub_clave = input(f"Ingrese el nombre de la subclave: ")
+                                        if not sub_clave:  # Si el usuario no ingresa nada 
+                                            break  # Sale del bucle de subclaves
+                                        sub_valor = input(f"Ingrese el valor para la subclave '{sub_clave}': ")
+                                        Dato_N[clave][sub_clave] = sub_valor  # Agrega la subclave y su valor al diccionario 
+            datos.append(Dato_N)  # Agrega el nuevo diccionario Dato_N a la lista datos
             print(f"Se ha creado un nuevo dato en el type '{tipo}'")
-            
 
         elif opcion == 3: # Actualizar datos 
             for i in datos:#se repite en la lista datos
@@ -57,13 +67,24 @@ while goku: #se abre while mientras goku sea true
                                 i[clave] = Valor_N #se agrega el nuevo valor a la clave
             print(f"Se han actualizado los datos del type '{tipo}'")
 
-        elif opcion == 4:# Eliminar 
-            D_actualizado = [i for i in datos if i['type'] != tipo] # Crea una nueva lista D_actualizado que contiene todos los diccionarios de la lista 'type' no coincide con el valor de la variable tipo
-            if len(datos) == len(D_actualizado):#Compara la longitud de las listas datos y D_actualizado Si son iguales significa que no se eliminó ningún elemento
-                print(f"No se encontraron datos para el type '{tipo}'") #imprime la informacion de type
-            else:#Si la longitud de las listas datos y D_actualizado es diferente significa que se eliminaron algunos elementos
-                datos[:] = D_actualizado #le da a la lista D_actualizado a la lista datos, reemplazando los elementos de datos por la nueva lista sin los elementos eliminados.
-                print(f"Se han eliminado los datos del type '{tipo}'") #Imprime un mensaje que seelimino
+        elif opcion == 4:  # Eliminar
+            id_eliminar = input(f"Ingrese el ID del dato '{tipo}': ")  # Solicita al usuario el ID del dato a eliminar
+
+            eliminado = False  #boleano
+            D_actualizado = []  # Lista para almacenar los datos actualizados
+
+            for i in datos:  # Itera sobre la lista de datos
+                if i['type'] == tipo:  # Si el valor de la clave 'type' coincide con el tipo seleccionado
+                    if str(i.get('id', '')) == id_eliminar:  # Si el valor de la clave 'id' coincide con el ID ingresado
+                        eliminado = True  # cierro el boleano y dice que se elimino un dato
+                    else:
+                        D_actualizado.append(i)  # Agrega el dato a la lista actualizada si no coincide con el ID
+
+            if eliminado:
+                datos[:] = D_actualizado  # Actualiza la lista de datos original con la lista actualizada
+                print(f"Se ha eliminado el dato con ID '{id_eliminar}' del type '{tipo}'")
+            else:
+                print(f"No se encontró ningún dato con ID '{id_eliminar}' en el type '{tipo}'")
 
         elif opcion == 5: #opcion 5 salir
             # Salir
@@ -73,7 +94,7 @@ while goku: #se abre while mientras goku sea true
             print("Opcion no valida") #si no ingresa un numero del 1 al 5 me sale este print
 
         # Guardar datos en el archivo JSON
-        with open('jaime.json', 'w', encoding='utf-8') as f:
+        with open('barrera.json', 'w', encoding='utf-8') as f:
             json.dump(datos, f)
 
     else:
